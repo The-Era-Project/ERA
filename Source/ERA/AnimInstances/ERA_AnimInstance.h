4ca1fe8b62a8f94ee6b82f16d0b26e6334fab868
Stark10 <sam@deeptree.co.nz>
2023-04-07 10:52:52 +1200

    Implement functionality to enable controller based movement.

---
+++ Source/ERA/AnimInstances/ERA_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ERA_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ERA_API UERA_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
